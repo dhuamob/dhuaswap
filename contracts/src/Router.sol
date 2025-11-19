88103a5023b3f5b1a345c2f3ecab1231199dc613
dhuamob <dhua.mobile@gmail.com>
2025-11-19 01:47:19 -0800

    feat: swap functionality implemented in router

--- contracts/src/Router.sol
+++ contracts/src/Router.sol
@@ -74,6 +74,59 @@ contract Router {
         address[] calldata path,
         address to
     ) external returns (uint[] memory amounts) {
-        revert("Router: not implemented");
+        require(path.length >= 2, "Router: INVALID_PATH");
+        require(to != address(0), "Router: INVALID_TO");
+
+        amounts = new uint[](path.length);
+        amounts[0] = amountIn;
+
+        address pairAddr = Factory(factory).getPair(path[0], path[1]);
+        require(pairAddr != address(0), "Router: PAIR_NOT_FOUND");
+        IERC20(path[0]).transferFrom(msg.sender, pairAddr, amountIn);
+
+        for (uint i = 0; i < path.length - 1; i++) {
+            address input = path[i];
+            address output = path[i + 1];
+            pairAddr = Factory(factory).getPair(input, output);
+            require(pairAddr != address(0), "Router: PAIR_NOT_FOUND");
+
+            uint amountOut = _getAmountOut(amounts[i], pairAddr, input);
+            amounts[i + 1] = amountOut;
+
+            (address token0,) = _sortTokens(input, output);
+            (uint amount0Out, uint amount1Out) = input == token0 ? (uint(0), amountOut) : (amountOut, uint(0));
+            address recipient = i < path.length - 2 ? Factory(factory).getPair(output, path[i + 2]) : to;
+            require(recipient != address(0), "Router: RECIPIENT_PAIR_NOT_FOUND");
+
+            Pair(pairAddr).swap(amount0Out, amount1Out, recipient);
+        }
+
+        require(amounts[path.length - 1] >= amountOutMin, "Router: INSUFFICIENT_OUTPUT");
+        address lastPair = Factory(factory).getPair(path[path.length - 2], path[path.length - 1]);
+        emit Swap(msg.sender, path[0], path[path.length - 1], amountIn, amounts[path.length - 1], lastPair);
+    }
+
+    function _getOrCreatePair(address tokenA, address tokenB) internal returns (address pairAddr) {
+        pairAddr = Factory(factory).getPair(tokenA, tokenB);
+        if (pairAddr == address(0)) {
+            pairAddr = Factory(factory).createPair(tokenA, tokenB);
+        }
+    }
+
+    function _sortTokens(address tokenA, address tokenB) private pure returns (address token0, address token1) {
+        require(tokenA != tokenB, "Router: IDENTICAL_ADDRESSES");
+        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), "Router: ZERO_ADDRESS");
+    }
+
+    function _getAmountOut(uint amountIn, address pairAddr, address input) private view returns (uint amountOut) {
+        (uint112 reserve0, uint112 reserve1,) = Pair(pairAddr).getReserves();
+        (uint reserveIn, uint reserveOut) = input == Pair(pairAddr).token0() ? (reserve0, reserve1) : (reserve1, reserve0);
+        require(amountIn > 0 && reserveIn > 0 && reserveOut > 0, "Router: INSUFFICIENT_LIQUIDITY");
+
+        uint amountInWithFee = amountIn * FEE_NUMERATOR;
+        uint numerator = amountInWithFee * reserveOut;
+        uint denominator = reserveIn * FEE_DENOMINATOR + amountInWithFee;
+        amountOut = numerator / denominator;
     }
 }
